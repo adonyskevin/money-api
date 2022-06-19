809badce91fa6d5d593ba84ff97a22430a1265cd
Adonys Kevin <adonyskevinms@gmail.com>
2022-06-19 11:39:57 -0300

    Refatorando o Location dos métodos salvar com Event

--- src/main/java/com/example/money/api/resource/PessoaResource.java
+++ src/main/java/com/example/money/api/resource/PessoaResource.java
@@ -1,12 +1,14 @@
 package com.example.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.money.api.event.RecursoCriadoEvent;
 import com.example.money.api.model.Pessoa;
 import com.example.money.api.repository.PessoaRepository;
 
@@ -25,6 +27,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Pessoa> listar(){
@@ -39,12 +44,10 @@ public class PessoaResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa) {
+	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 	
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
-				.path("/{codigo}").buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 }
